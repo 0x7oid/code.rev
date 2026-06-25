d4eb3af67e3ed6d97aedad82549df045e363ae1c
0x7oid <ryadgm.pro@gmail.com>
2026-06-25 20:37:34 +0100

    sat up llm client (default is gemini 3.5 flash)

---
+++ src/llm/llm_client.py
@@ -0,0 +1,12 @@
+from google import genai
+import os
+
+client = genai.client(api_key=os.environ.get("API_KEY"))
+
+
+def ask_llm(prompt , model="gemini-3.5-flash"):
+    response = client.models.generate_content(
+        model=model , contents = prompt
+    )
+    return response.text
+
